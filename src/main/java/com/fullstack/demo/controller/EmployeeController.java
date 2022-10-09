136965f28dd33f568ffd5347dd1f522a6df449fe
Nagoor-Khan-P <106923967+Nagoor-Khan-P@users.noreply.github.com>
2022-10-09 11:18:54 +0530

    implemented save employee method

--- src/main/java/com/fullstack/demo/controller/EmployeeController.java
+++ src/main/java/com/fullstack/demo/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.fullstack.demo.controller;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fullstack.demo.dto.EmployeeRequestDTO;
 import com.fullstack.demo.dto.EmployeeResponseDTO;
+import com.fullstack.demo.entity.Employee;
+import com.fullstack.demo.service.EmployeeService;
 
 
 @RestController
 @RequestMapping("/employees") // to map the controller
 public class EmployeeController {
 	
+	@Autowired
+	private EmployeeService employeeService;//it will give the EmployeeServiceImpl object injected in IOC container
+	
+	@Autowired
+	private ModelMapper mapper;
+//	it will give the mapper object wich converts DTO to Entity classes
+	
 	@PostMapping //this annotation is used to define the post method
 	public EmployeeResponseDTO saveEmployee(@RequestBody EmployeeRequestDTO emploDto) {
-		return null;
+		Employee emp=mapper.map(emploDto, Employee.class);
+		Employee employee = employeeService.saveEmployee(emp);
+		EmployeeResponseDTO employeeResponseDTO=mapper.map(employee, EmployeeResponseDTO.class);
+		return employeeResponseDTO;
 	}
 	
 	@GetMapping
